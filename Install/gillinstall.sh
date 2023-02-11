81e9410b710362c42ba9d65e53c5d6acbe588c54
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-02-11 05:09:12 +0300

    Update gillinstall.sh

--- Install/gillinstall.sh
+++ Install/gillinstall.sh
@@ -2,11 +2,11 @@ clear
 echo "------------------------------------"
 echo " WELCOME TO DOLPHIN GILL INSTALLER "
 echo "------------------------------------"
-echo "Do you want to install nodejs? Y/N"
 echo "Installing nodejs@16"
 sudo apt update -y
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
 sudo apt install nodejs -y
+echo "Installing qemu"
 sudo apt-get update -y
 sudo apt-get install qemu  -y
 clear
@@ -17,5 +17,4 @@ clear
 echo ------------------------------------
 echo The installation is complete, you can go to the main directory and run npm start
 echo ------------------------------------
-cd ..
 npm start
