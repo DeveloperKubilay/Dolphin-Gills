8c5ed3af33692cc0f4fec805afb83b71de361b28
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-02-11 05:08:54 +0300

    Update gillinstall.bat

--- Install/gillinstall.bat
+++ Install/gillinstall.bat
@@ -4,57 +4,44 @@ cls
 echo ------------------------------------
 echo  WELCOME TO DOLPHIN GILL INSTALLER
 echo ------------------------------------
-set /p winlater="Are you using Windows 10 or Windows Server 2019 or Later? Y/N: "
-set /p nodejsinstall="Do you want to install nodejs? Y/N: "
+set /p winlater="If you are using Windows 10 or Windows Server 2019 or Later, do you have to install nodejs and qemu automatically? Y/N: "
+set /p nodejsinstall="Do you want to download normally (If you said yes, you don't need to say it again.) Y/N: "
 
-if /i "%nodejsinstall%" == "yes" goto nodejsinstall
-if /i "%nodejsinstall%" == "Y" goto nodejsinstall
-if /i "%nodejsinstall%" == "Yes" goto nodejsinstall
-if /i "%nodejsinstall%" == "y" goto nodejsinstall
-
-:nodejsinstall
 if "%winlater%" == "y" (
 winget install --id=OpenJS.NodeJS.LTS  -e
+winget install --id=SoftwareFreedomConservancy.QEMU  -e
 ) else if "%winlater%" == "Y" (
 winget install --id=OpenJS.NodeJS.LTS  -e
+winget install --id=SoftwareFreedomConservancy.QEMU  -e
 ) else if "%winlater%" == "Yes" (
 winget install --id=OpenJS.NodeJS.LTS  -e
+winget install --id=SoftwareFreedomConservancy.QEMU  -e
 ) else if "%winlater%" == "yes" (
 winget install --id=OpenJS.NodeJS.LTS  -e
-) else (
+winget install --id=SoftwareFreedomConservancy.QEMU  -e
+) else if "%nodejsinstall%" == "y" (
 start https://nodejs.org/en/download/
+start https://www.qemu.org/download/#windows
 echo After installing please press a button
 pause
-)
-
-set /p qemuinstall="Do you want to install qemu? Y/N: "
-
-if /i "%qemuinstall%" == "yes" goto qemuinstall
-if /i "%qemuinstall%" == "Y" goto qemuinstall
-if /i "%qemuinstall%" == "Yes" goto qemuinstall
-if /i "%qemuinstall%" == "y" goto qemuinstall
-
-:qemuinstall
-if "%winlater%" == "y" (
-winget install --id=SoftwareFreedomConservancy.QEMU  -e
-) else if "%winlater%" == "Y" (
-winget install --id=SoftwareFreedomConservancy.QEMU  -e
-) else if "%winlater%" == "Yes" (
-winget install --id=SoftwareFreedomConservancy.QEMU  -e
-) else if "%winlater%" == "yes" (
-winget install --id=SoftwareFreedomConservancy.QEMU  -e
-) else (
+) else if "%nodejsinstall%" == "Y" (
+start https://nodejs.org/en/download/
+start https://www.qemu.org/download/#windows
+echo After installing please press a button
+pause
+) else if "%nodejsinstall%" == "yes" (
+start https://nodejs.org/en/download/
+start https://www.qemu.org/download/#windows
+echo After installing please press a button
+pause
+) else if "%nodejsinstall%" == "Yes" (
+start https://nodejs.org/en/download/
 start https://www.qemu.org/download/#windows
 echo After installing please press a button
 pause
 )
+
 cls
 node gillinstaller.js
 cd ..
-npm install
-cls
-echo ------------------------------------
-echo The installation is complete, you can go to the main directory and run npm start
-echo ------------------------------------
-cd ..
-npm start
+npm install && cls && npm start
